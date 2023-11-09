da127fcd2f6cae1fe73fc858d686d6e7328a0228
jlucasbx <jlucasb16@gmail.com>
2023-11-09 14:33:30 -0300

    alerting users

--- front-end/src/utils/getProducts.js
+++ front-end/src/utils/getProducts.js
@@ -2,7 +2,13 @@
 // Logic to fetch information from the backend
 export default async function getProducts(search=""){
     if(!search) return [];
-    const response = await fetch(`http://127.0.0.1:3000/api/scrape?keyword=${search}`);
-    const data  = await response.json();
-    return data;
+
+    try{
+        const response = await fetch(`http://127.0.0.1:3000/api/scrape?keyword=${search}`);
+        const data  = await response.json();
+        return data;
+    }catch{
+        alert("Unable to establish an internet connection. Please check your network settings and try again.");
+        return  [];
+    }
 }
